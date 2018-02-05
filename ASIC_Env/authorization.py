db37ce2dacaadb16700ce97efaf37cd7fd5c156f
Nathan Nguyen <nathannguyen345@gmail.com>
2018-02-05 13:34:10 -0500

    clean up code

--- ASIC_Env/authorization.py
+++ ASIC_Env/authorization.py
@@ -1,7 +1,13 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    This is our authorization process. It stores all the application keys as global variables. There's two
+    functions that associate itself with this file. Authorization for permission to access the adobe application
+    on the user's behalf and token retrieval.
+    Written By: Nathan Nguyen
+    Date: 2/2/2018
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 from requests_oauthlib import OAuth2Session
 
-
-# Application configutation
+# Global variables for application configuration
 client_id     = 'CBJCHBCAABAAgAfIaHGyanFH3GBNZMVk94AGRkagLoh-'
 client_secret = 'PbV0cUkRruvJqRi7-yFnCra12BXm7XjX'
 redirect_uri  = 'https://example.com'
@@ -12,6 +18,11 @@ oauth         = ""
 
 
 def authorization():
+    """
+    This function will authorize the client with the adobe server. A call back token is needed to
+    confirm that the user has permission to access the server on behalf of the sender.
+    :return: auth_url: authorization link to give client access and permission to the server
+    """
 
     global auth_url
     global oauth
@@ -26,6 +37,11 @@ def authorization():
     return auth_url
 
 def get_token(auth_responses):
+    """
+    This function will return the the access token to the client to be saved and use for api calls
+    :param auth_responses:
+    :return: token: Access token to be used for api calls
+    """
 
     # Fetch access token
     token = oauth.fetch_token(token_url=token_url, authorization_response=auth_responses,
--- ASIC_Env/main.py
+++ ASIC_Env/main.py
@@ -1,3 +1,9 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    This is a flask micro-framework that will integrate the client side web application to integrate with adobe
+    echo sign backend server with their api. 
+    Written By: Nathan Nguyen
+    Date: 2/2/2018
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from werkzeug.utils import secure_filename
@@ -5,24 +11,24 @@ import requests
 import authorization
 import agreements
 
-# Set upload path and allowed extensions
-UPLOAD_FOLDER = '/pdf/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'jpg', 'png', 'doc', 'docx', 'gif', 'rtf'])
-
+# Flask config set up
 app = Flask(__name__)
+APP_ROOT = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, '/instance')
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'jpg', 'png', 'doc', 'docx', 'gif', 'rtf'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Global Decration
+# Global authorization declaration
 auth_url = authorization.authorization()
-header = ''
-transcientID = ''
-email = ''
+
+# Global variable that will store all of our cookies during the session
+session = {}
 
 # Route to the authorization page
 @app.route('/', methods=['GET', 'POST'])
 def oauth():
 
-    global header
+    global session
 
     # If user clicks get token button
     if request.method == "POST":
@@ -33,34 +39,27 @@ def oauth():
         # Fetch the access token information
         token = authorization.get_token(auth_responses)
 
-        header = {'Access-Token': '{}'.format(token['access_token'])}
-        base_uris = requests.get('https://api.na2.echosign.com//api/rest/v5/base_uris', headers=header)
+        session['header'] = {'Access-Token': '{}'.format(token['access_token'])}
 
         # Redirect to the user to the send document page if token is valid
         if token:
-            return redirect(url_for('upload_doc'))
+            return redirect(url_for('upload_doc', session=session))
 
-    return render_template('oauth.html', auth_url = auth_url, header=header)
-
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+    return render_template('oauth.html', auth_url = auth_url, session=session)
 
 
 # Route to the upload document page
 @app.route('/upload_doc', methods=['GET', 'POST'])
 def upload_doc():
 
-    global transcientID
-    global email
+    global app
 
+    # If the user clicks submit button
     if request.method == "POST":
 
         # Extract recipients email address
         email = request.form['email']
-        email = email.split(' ')
+        session['email'] = email.split(' ')
 
         # Extract the pdf file that the user and secure save it to the file system
         f = request.files['file']
@@ -68,23 +67,21 @@ def upload_doc():
 
         # Form data for api call
         data = {
-            'Mine-Type': 'application/pdf',
-            'File-Name' : 'PDF File'
+            'Mine-Type': 'application/rtf',
+            'File-Name': 'RTF File'
         }
 
         # File data for api call
-        # pdf_file = {'File': open('test_doc.pdf', 'rb')}
-        pdf_file = {'File': open('test.rtf', 'rb')}
+        doc_file = {'File': open(f.filename, 'rb')}
 
         # Post transientDocument and get document ID
-        transcientID = requests.post('https://api.na2.echosign.com/api/rest/v5/transientDocuments',
-                                     headers=header,
-                                     data=data,
-                                     files=pdf_file).json().get('transientDocumentId')
+        session['transientID'] = requests.post('https://api.na2.echosign.com/api/rest/v5/transientDocuments',
+                                               headers=session['header'],
+                                               data=data,
+                                               files=doc_file).json().get('transientDocumentId')
 
-        if transcientID:
-            return redirect((url_for('send_doc')))
-
+        if session['transientID']:
+            return redirect((url_for('send_doc', session=session)))
 
     return render_template('upload_doc.html')
 
@@ -97,13 +94,13 @@ def send_doc():
     agreementID_records = {}
 
     # Iterate through email list and request a post call to send out documents
-    for recipient in email:
-        agreement = agreements.create_agreement(transcientID, recipient)
+    for recipient in session['email']:
+        agreement = agreements.create_agreement(session['transientID'], recipient)
         agreementID = requests.post('https://api.na2.echosign.com:443/api/rest/v5/agreements',
-                                    headers=header,
+                                    headers=session['header'],
                                     json=agreement).json().get('agreementId')
 
-        # Store all reciept's email address along with their agreement ID's
+        # Store all recipient's email address along with their agreement ID's
         agreementID_records[recipient] = agreementID
 
     return render_template('send_doc.html')
